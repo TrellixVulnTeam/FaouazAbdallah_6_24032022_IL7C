4317560c96798644fb25e1694ea8222efec8ec8b
GitHubFaouaz <faouaza@yahoo.com>
2022-04-27 16:35:12 +0200

    securité

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
 
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },//unique:true pour ne pas que plusieurs User utilise la meme adresse mail pour l'inscription
   password: { type: String, required: true }
 });
-// prévalider les informations avant de les enregistrer (pour ne pas avoir des erreurs ilisibles de la part de mongooseDB)
+// Applique ce validator au userSchema avant d'en faire un model (pour ne pas avoir des erreurs ilisibles de la part de mongooseDB)
 userSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model('User', userSchema); //User.js 
+// Ce plugin tire parti des middlewares de gestion des erreurs récemment introduits sur mongoose , il identifie avec succès le chemin (ou le champ) défaillant et la valeur correctement, et les ajoute dans le hachage des erreurs
+userSchema.plugin(mongodbErrorHandler);
+
+
+module.exports = mongoose.model('User',userSchema); 
